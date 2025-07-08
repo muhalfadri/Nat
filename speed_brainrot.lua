9c4ff395c1482ade0de7a85fa00cdd1ae9f436a3
muhalfadri <muhalfadri71@gmail.com>
2025-07-08 10:12:24 +0800

    Create speed_brainrot.lua

---
+++ speed_brainrot.lua
@@ -0,0 +1,6 @@
+local player = game.Players.LocalPlayer
+local char = player.Character or player.CharacterAdded:Wait()
+local humanoid = char:WaitForChild("Humanoid")
+
+humanoid.WalkSpeed = 100
+print("⚡ Speed diatur ke " .. humanoid.WalkSpeed)
